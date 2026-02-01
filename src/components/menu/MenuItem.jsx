3febe00733acf5432e4ca77a2b38b99218b3a655
AIT MOUHEB Arezki <ait.mouheb.arezki@gmail.com>
2026-02-01 16:10:33 +0100

    improve CSS for Menu page

--- src/components/menu/MenuItem.jsx
+++ src/components/menu/MenuItem.jsx
@@ -1,11 +1,18 @@
 const STATIC_IMAGES_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export default function MenuItem({ pizza }){
-    return(
-        <div key={pizza.id} className="menu-item">
-          <img src={STATIC_IMAGES_BASE_URL + pizza.image_path} alt={pizza.name} style={{ width: "100px", height: "100px" }} />
-          <div className="menu-item-name">{pizza.name}</div>
-          <div className="menu-item-description">{pizza.description}</div>
-          <div className="menu-item-price">{pizza.price}</div>
+export default function MenuItem({ pizza }) {
+    return (
+        <div key={pizza.id} className="card menu-item">
+            <div className="menu-item-img">
+                <img src={STATIC_IMAGES_BASE_URL + pizza.image_path} alt={pizza.name} />
+            </div>
+            <div className="menu-item-details">
+                <div className="menu-item-name">{pizza.name}</div>
+                <div className="menu-item-description">{pizza.description}</div>
+                <div className="menu-item-price">{pizza.price}€</div>
+                <div className="menu-item-action">
+                    <button>Add</button>        
+                </div>
+            </div>
         </div>
     );
 
