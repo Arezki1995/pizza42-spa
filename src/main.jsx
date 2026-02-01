46d579a4b0da2ed748591797c34e40f38003d77d
AIT MOUHEB Arezki <ait.mouheb.arezki@gmail.com>
2026-02-01 20:54:08 +0100

    install and initialize auth0 Provider (using refresh tokens & local storage for caching for the moment)

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from './context/CartContext.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './assets/css/index.css'
 import App from './app/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CartProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </CartProvider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+      cacheLocation="localstorage"
+      useRefreshTokens={true}
+    >
+      <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CartProvider>
+    </Auth0Provider>
+
   </StrictMode>
 )
