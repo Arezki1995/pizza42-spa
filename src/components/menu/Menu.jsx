65bc836692a49244458ec68e562886ff2b96ba2e
AIT MOUHEB Arezki <ait.mouheb.arezki@gmail.com>
2026-02-01 21:11:01 +0100

    fix key issue in menu list

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,7 +4,7 @@ export default function Menu({ menu }){
   return (
     <div className="pizza-menu-list">
       {menu.map(pizza => (
-        <MenuItem pizza={pizza} />
+        <MenuItem key={pizza.id} pizza={pizza} />
       ))}
     </div>
 
