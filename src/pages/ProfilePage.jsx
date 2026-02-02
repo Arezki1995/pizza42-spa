26a5cc031817cd70ce07c32106322ef71fd482d9
AIT MOUHEB Arezki <ait.mouheb.arezki@gmail.com>
2026-02-02 20:20:46 +0100

    component naming consistency

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import Profile from "../components/profile/profile";
+import Profile from "../components/profile/Profile";
 import OrdersHistory from "../components/orders/OrderHistory";
 
 export default function ProfilePage() {
