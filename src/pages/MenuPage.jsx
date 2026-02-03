3f8948de9739beefcfa0cff7c4bb4d567038c27d
AIT MOUHEB Arezki <ait.mouheb.arezki@gmail.com>
2026-02-03 23:23:58 +0100

    add cart shortcut on menu page

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useCart } from "../context/CartContext";
 import { fetchMenu } from "../services/menuApi";
 import Menu from "../components/menu/Menu";
 import Message from "../components/nav/Message";
@@ -7,6 +9,8 @@ function MenuPage() {
   const [menu, setMenu] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const { isEmpty, totalItems } = useCart();
 
   useEffect(() => {
     fetchMenu()
@@ -33,6 +37,10 @@ function MenuPage() {
     <>
       <div className="card menu-title">Menu</div>
       <Menu menu={menu} />  
+      {!isEmpty && (
+        <Link to="/cart" className="cart-shortcut" > Go to Cart<span className="cart-shortcut-count">{totalItems}</span>
+        </Link>
+      )}
     </>
   );
 }
