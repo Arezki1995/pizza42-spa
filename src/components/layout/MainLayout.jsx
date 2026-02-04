fb39cc0362cde416a9b790f964f4a42a6e5f21de
AIT MOUHEB Arezki <ait.mouheb.arezki@gmail.com>
2026-02-04 17:05:20 +0100

    basic banner to indicate that email is not yet verified and required

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,12 +1,28 @@
 import { Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import Navbar from "./NavBar";
 import Footer from "./Footer";
 
+
+
 export default function MainLayout() {
+    const { isAuthenticated, user } = useAuth0();
     return (
         <div className="layout">
-            <Navbar/>
+            <Navbar />
+            {
+             (isAuthenticated && !user.email_verified) ?
+                (
+                <div className="email-verification-banner">
+                    An email is sent to mailbox to finalize your account setup.
+                    A verified email <b>is required</b> in order to place an Order.
+                </div>
+                ) : null
+
+            }
+
             <main className="container">
+
                 <Outlet />
             </main>
             <Footer />
